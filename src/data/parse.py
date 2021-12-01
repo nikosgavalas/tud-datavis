366b29679cb827c4457315bd457beba60ca5c654
Nikos Gavalas <gavalnick@gmail.com>
2021-12-02 00:05:14 +0100

    Make map functional

---
+++ src/data/parse.py
@@ -0,0 +1,21 @@
+import csv
+import json
+
+WRITE_CSV = False
+
+country_popul = {}
+with open('./data.csv', 'r') as inf, open('./map-data.csv', 'w') as outf:
+    reader = csv.reader(inf, delimiter=',', quotechar='"')
+    if WRITE_CSV:
+        writer = csv.writer(outf, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['country_code'] + ['_' + str(i)
+                                            for i in range(2020, 1959, -1)])
+    for row in reader:
+        if row[3] == 'SP.POP.1564.TO.ZS':
+            country_popul[row[1]] = row[4:]
+            if WRITE_CSV:
+                writer.writerow([row[1]] + row[4:])
+
+with open('./map-data.json', 'w') as outf:
+    outf.write(json.dumps(country_popul))
--- src/js/map.js
+++ src/js/map.js
@@ -8,13 +8,42 @@ const projection = d3
   .translate([width / 2, height / 2]);
 const path = d3.geoPath().projection(projection);
 
-d3.json("./json/world.geojson").then(function (data) {
-  svg
-    .append("g")
-    .selectAll("path")
-    .data(data.features)
-    .join("path")
-    .attr("fill", "#69b3a2")
-    .attr("d", path)
-    .style("stroke", "#fff");
+const color = d3.scaleLinear().domain([50, 75]).range(["blue", "white"]);
+
+var countries;
+d3.json("./data/map-data.json").then((data) => {
+  countries = data;
+
+  d3.json("./data/world.geojson").then(function (data) {
+    var map = svg
+      .append("g")
+      .selectAll("path")
+      .data(data.features)
+      .join("path")
+      .attr("d", path)
+      .attr("fill", (d, i) => {
+        var value = d.id in countries ? countries[d.id][0] : 0;
+        if (value == "..") {
+          value = 0;
+        }
+        return color(value);
+      })
+      .style("stroke", "#fff");
+
+    function changeSize(size) {
+      map.attr("fill", (d, i) => {
+        var idx = 2020 - size;
+        var value = d.id in countries ? countries[d.id][idx] : 0;
+        if (value == "..") {
+          value = 0;
+        }
+        return color(value);
+      });
+    }
+
+    d3.select("#timeSlider").on("change", function (d) {
+      selectedValue = this.value;
+      changeSize(selectedValue);
+    });
+  });
 });
